40d8f4a6d68bd4ab18f7ac32ee2575038d03a5a4
vignesh1999 <vinu6th@gmail.com>
2021-09-09 10:46:26 +0530

    added toast

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -2,16 +2,30 @@ import './styles.css'
 import ComponentContainer from '../Component_container'
 
 export default function Toast() {
+  function CreateNotification() {
+    const toasts = document.getElementById('toasts')
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+    notif.innerHTML = 'Hi there!!'
+    toasts.appendChild(notif)
+
+    setTimeout(() => {
+      notif.remove()
+    }, 3000)
+  }
   return (
     <div>
       <ComponentContainer>
         <div className='component-heading' id='avatar-heading'>
           Toast
         </div>
-        <div class='toast'>Disguised Toast</div>
+        <div id='toasts'></div>
+        <button class='btn' onClick={CreateNotification}>
+          Show Toast
+        </button>
         <iframe
           title='toast'
-          src='https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520class%253D%27toast%27%253EDisguised%2520Toast%253C%252Fdiv%253E'
+          src='https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520class%253D%27toast%27%253EHi%2520There!!%253C%252Fdiv%253E'
           style={{ transform: 'scale(1)', overflow: 'hidden', height: '100px' }}
           sandbox='allow-scripts allow-same-origin'
         ></iframe>
@@ -19,20 +33,3 @@ export default function Toast() {
     </div>
   )
 }
-
-// function CreateNotification() {
-//   const toasts = document.getElementById('toasts')
-//   const notif = document.createElement('div')
-//   notif.classList.add('toast')
-//   notif.innerHTML = 'Droplet UI'
-//   toasts.appendChild(notif)
-
-//   setTimeout(() => {
-//     notif.remove()
-//   }, 3000)
-// }
-
-/* <div id='toasts'></div>
-        <button class='btn' onClick={CreateNotification}>
-          Show Notification
-        </button> */
